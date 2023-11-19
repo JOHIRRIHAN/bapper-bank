ec4cd2386c05f7c8a31c27d17c0356756ca8eafa
JOHIRRIHAN <jr7718301@gmail.com>
2023-11-19 23:40:26 +0600

    second commit

---
+++ js/deposit.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    
+    const depositField= document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    const depositTotalElement= document.getElementById('deposit-total');
+    const previousDepositTotalstring = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalstring);
+    
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText=currentDepositTotal;
+    depositField.value='';
+})
